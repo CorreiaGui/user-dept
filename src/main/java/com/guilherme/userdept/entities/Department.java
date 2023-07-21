b4022c40b72b1c3df515a970f238c53c06d444d5
Guilherme Correia <guilherme.martins@contmatic.com.br>
2023-07-21 15:28:51 -0300

    trocando getters e setters por lombok

--- src/main/java/com/guilherme/userdept/entities/Department.java
+++ src/main/java/com/guilherme/userdept/entities/Department.java
@@ -5,7 +5,13 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "tb_department")
 public class Department {
@@ -16,21 +22,4 @@ public class Department {
 	
 	private String name;
 	
-	public Department() {}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
 }
--- src/main/java/com/guilherme/userdept/entities/User.java
+++ src/main/java/com/guilherme/userdept/entities/User.java
@@ -7,7 +7,13 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
 @Table(name = "tb_user")
 public class User {
@@ -22,38 +28,4 @@ public class User {
 	@JoinColumn(name = "department_id")
 	private Department department;
 	
-	public User() {
-	}
-
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public String getEmail() {
-		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public Department getDepartment() {
-		return department;
-	}
-
-	public void setDepartment(Department department) {
-		this.department = department;
-	}
 }
